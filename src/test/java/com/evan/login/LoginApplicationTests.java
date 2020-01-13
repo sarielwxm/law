f76f60e6f7896298983161c898b680eb83edf783
sarielwxm <40795114+sarielwxm@users.noreply.github.com>
2020-01-13 11:19:13 +0800

    law

---
+++ src/test/java/com/evan/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evan.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
